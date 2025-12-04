f0cfbde632712ee2388a309fa863a84517ffebe1
Uzoma Obinwanne <uzomaobinwanne@gmail.com>
2025-12-04 14:18:58 -0600

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+
 import CaroselBanner from "@/components/CaroselBanner";
 import MovieContainer from "@/components/MovieContainer";
 import {
@@ -8,20 +9,45 @@ import {
 } from "@/lib/getMovies";
 
 export default async function Home() {
-  const nowPlayingMovies = await getNowPlayingMovies();
-  const upcomingMovies = await getUpcomingMovies();
-  const topRatedMovies = await getTopRatedMovies();
-  const popularMovies = await getPopularMovies();
-  
-  return (
-    <main>
-      <CaroselBanner />
-      <div className="flex flex-col space-y-2">
-        <MovieContainer movies={nowPlayingMovies} title="Now Playing" />
-        <MovieContainer movies={upcomingMovies} title="Upcoming" />
-        <MovieContainer movies={topRatedMovies} title="Top Rated" />
-        <MovieContainer movies={popularMovies} title="Popular" />
-      </div>
-    </main>
-  );
+  try {
+    const [nowPlayingMovies, upcomingMovies, topRatedMovies, popularMovies] =
+      await Promise.all([
+        getNowPlayingMovies().catch(() => []),
+        getUpcomingMovies().catch(() => []),
+        getTopRatedMovies().catch(() => []),
+        getPopularMovies().catch(() => []),
+      ]);
+
+    return (
+      <main>
+        <CaroselBanner />
+        <div className="flex flex-col space-y-2">
+          {nowPlayingMovies?.length > 0 && (
+            <MovieContainer movies={nowPlayingMovies} title="Now Playing" />
+          )}
+          {upcomingMovies?.length > 0 && (
+            <MovieContainer movies={upcomingMovies} title="Upcoming" />
+          )}
+          {topRatedMovies?.length > 0 && (
+            <MovieContainer movies={topRatedMovies} title="Top Rated" />
+          )}
+          {popularMovies?.length > 0 && (
+            <MovieContainer movies={popularMovies} title="Popular" />
+          )}
+        </div>
+      </main>
+    );
+  } catch (error) {
+    console.error("Error loading movies:", error);
+    return (
+      <main>
+        <CaroselBanner />
+        <div className="flex flex-col space-y-2">
+          <p className="text-center text-gray-400 py-10">
+            Unable to load movies. Please try again later.
+          </p>
+        </div>
+      </main>
+    );
+  }
 }
