985bd0b467a0420d020dfb2af964506b94ba98aa
Uzoma Obinwanne <uzomaobinwanne@gmail.com>
2025-12-04 14:56:06 -0600

    fix getMovies and layout

--- src/lib/getMovies.ts
+++ src/lib/getMovies.ts
@@ -1,5 +1,5 @@
-/* eslint-disable @typescript-eslint/no-unused-expressions */
 import { SearchResults } from "../../type";
+
 const fetcher = async (url: URL, cacheTime?: number) => {
   url.searchParams.set("include_adult", "false");
   url.searchParams.set("include_video", "false");
@@ -18,34 +18,54 @@ const fetcher = async (url: URL, cacheTime?: number) => {
     },
   };
 
-  const response = await fetch(url.toString(), options);
-  const data = (await response.json()) as SearchResults;
+  try {
+    const response = await fetch(url.toString(), options);
+    
+    if (!response.ok) {
+      console.error(`TMDB API Error: ${response.status} ${response.statusText}`);
+      return {
+        results: [],
+        page: 1,
+        total_pages: 0,
+        total_results: 0,
+      } as SearchResults;
+    }
 
-  return data;
+    const data = (await response.json()) as SearchResults;
+    return data;
+  } catch (error) {
+    console.error("Error fetching from TMDB:", error);
+    return {
+      results: [],
+      page: 1,
+      total_pages: 0,
+      total_results: 0,
+    } as SearchResults;
+  }
 };
 
 export const getNowPlayingMovies = async () => {
   const url = new URL("https://api.themoviedb.org/3/movie/now_playing");
   const data = await fetcher(url);
-  return data.results;
+  return data.results || [];
 };
 
 export const getUpcomingMovies = async () => {
   const url = new URL("https://api.themoviedb.org/3/movie/upcoming");
   const data = await fetcher(url);
-  return data.results;
+  return data.results || [];
 };
 
 export const getTopRatedMovies = async () => {
   const url = new URL("https://api.themoviedb.org/3/movie/top_rated");
   const data = await fetcher(url);
-  return data.results;
+  return data.results || [];
 };
 
 export const getPopularMovies = async () => {
   const url = new URL("https://api.themoviedb.org/3/movie/popular");
   const data = await fetcher(url);
-  return data.results;
+  return data.results || [];
 };
 
 export const getDiscoverMovies = async (id?: string, keywords?: string) => {
@@ -55,7 +75,7 @@ export const getDiscoverMovies = async (id?: string, keywords?: string) => {
   id && url.searchParams.set("with_genres", id);
 
   const data = await fetcher(url);
-  return data.results;
+  return data.results || [];
 };
 
 export const getSearchedMovies = async (term: string) => {
@@ -63,18 +83,19 @@ export const getSearchedMovies = async (term: string) => {
   url.searchParams.set("query", term);
 
   const data = await fetcher(url);
-  return data.results;
+  return data.results || [];
 };
 
 export const getMovieVideos = async (id?: string) => {
   const url = new URL(`https://api.themoviedb.org/3/movie/${id}/videos`);
 
   const data = await fetcher(url);
-  return data.results;
+  return data.results || [];
 };
+
 export const getMovieDetails = async (id?: string) => {
   const url = new URL(`https://api.themoviedb.org/3/movie/${id}`);
 
   const data = await fetcher(url);
   return data;
-};
+}
